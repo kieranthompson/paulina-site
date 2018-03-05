4d6f20761dd13720bd87328de61dde92fcee77ff
Kieran Thompson <l00118651@student.lyit.ie>
2018-03-05 21:30:30 +0000

    fixed map size and location

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var uluru = {lat: 51.756304, lng: 19.457239};
+    var uluru = {lat: 51.756424, lng: 19.460168};
     var map = new google.maps.Map(document.getElementById('map_id'), {
       zoom: 17,
       center: uluru
